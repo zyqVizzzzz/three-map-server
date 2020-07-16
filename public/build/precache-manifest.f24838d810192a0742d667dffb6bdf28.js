583f4aa1cd93829110308a67e897cf1e19cc66aa
zhongyuqiang <zhongyuqiang@mail.com>
2020-07-16 15:13:26 +0800

    init

---
+++ public/build/precache-manifest.f24838d810192a0742d667dffb6bdf28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9130cc75137fc8e45e00f04ed50cf84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a8329d2b411a549aa38",
+    "url": "/static/css/main.8c53a8d7.chunk.css"
+  },
+  {
+    "revision": "4085086059eeee1c0d89",
+    "url": "/static/js/2.4c80951a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c80951a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a8329d2b411a549aa38",
+    "url": "/static/js/main.8b2a3ae2.chunk.js"
+  },
+  {
+    "revision": "07d110fff3220ff10402",
+    "url": "/static/js/runtime-main.32e50cd1.js"
+  }
+]);
--- src/application.ts
+++ src/application.ts
@@ -22,7 +22,7 @@ export class ThreeMapServerApplication extends BootMixin(
     this.sequence(MySequence);
 
     // Set up default home page
-    this.static('/', path.join(__dirname, '../public'));
+    this.static('/', path.join(__dirname, '../public/build'));
 
     // Customize @loopback/rest-explorer configuration here
     this.configure(RestExplorerBindings.COMPONENT).to({
